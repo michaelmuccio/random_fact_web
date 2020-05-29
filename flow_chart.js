3e26a43470e70cee88f1cb3e6b97c5d2aeb9e95b
Mike Muccio <mike@2niteapp.com>
2020-05-29 11:51:41 -0700

    Add disclaimer footer and links

--- flow_chart.js
+++ flow_chart.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
 
     var yesButton = $('#yesButton');
-    var jumbotron = $('#jumbotron');
     var noButton = $('#noButton');
+    var disclaimerText = $('#disclaimerText');
     var randomFactContainer = $('#randomFactContainer');
+    var linkContainer = $('#link');
+    var buyStuffText =$('#buyStuffText');
     var randomIndex = 0;
+    var disneyLink = 'https://www.disney.com';
 
     var factList = ['There is a .04% chance you will die from Chinese Bat virus. You are a nerd!',
         'Yummy! Sharks eat humans more often than Covid-19 kills humans. Scaredy cat',
@@ -12,18 +15,36 @@ $(document).ready(function () {
         'Sick lamborgini bro. I know your unemployment check didn\'t pay for that. You will win the lottery before you die from Covid-19. Donny Nerdo',
         'Dang dude, your chick is a hottie. You will meet your soulmate at a bar before Covid-19 finds you and puts you in the grave. You nerd'];
 
+    var disclaimer = '*All comparisons are facts and provided by Matt Macniak'
+    var statList = [
+        new Stat('There is a .04% chance you will die from Chinese Bat virus. You are a nerd!', disclaimer, disneyLink),
+        new Stat('Yummy! Sharks eat humans more often than Covid-19 kills humans. Scaredy cat', disclaimer, disneyLink),
+        new Stat('Have fun getting struck by lightning, because that will happen before you die from Covid-19. You are a nerd!', disclaimer, disneyLink),
+        new Stat('Sick lamborgini bro. I know your unemployment check didn\'t pay for that. You will win the lottery before you die from Covid-19. Donny Nerdo', disclaimer, disneyLink),
+        new Stat('Dang dude, your chick is a hottie. You will meet your soulmate at a bar before Covid-19 finds you and puts you in the grave. You nerd', disclaimer, disneyLink),
+    ];
+
+    buyStuffText.hide();
+
     yesButton.on('click', function (e) {
         e.preventDefault();
         console.log('Yes clicked');
-        var randomFact = random_item(factList);
-        console.log(randomFact);
-        randomFactContainer.text(randomFact);
+        var randomFact = random_item(statList);
+        console.log(statList);
+        randomFactContainer.text(randomFact.stat);
+        disclaimerText.show();
+        buyStuffText.show();
+        disclaimerText.text(randomFact.disclaimer);
+        linkContainer.text('Monetized Link example');
+        linkContainer.attr("href", randomFact.relativeLinks);
     });
 
     noButton.on('click', function (e) {
         e.preventDefault();
         console.log('No clicked');
         randomFactContainer.text("Unfortunately there is no hope for you. Dig a bunker and live under gorund for the rest of your life");
+        disclaimerText.hide();
+        buyStuffText.show();
     });
 
     function random_item(items) {
@@ -36,6 +57,14 @@ $(document).ready(function () {
         }
 
         random_item(factList);
-
     }
+
 });
+
+class Stat {
+    constructor(stat, disclaimer, relativeLinks) {
+        this.stat = stat;
+        this.disclaimer = disclaimer;
+        this.relativeLinks = relativeLinks;
+    }
+}
